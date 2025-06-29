4e997fa8e64c26de6fe9c4bf3477e9342f68b3ca
Nurodayo <cianuroicecream@gmail.com>
2025-06-29 19:48:40 -0400

    no se si ya hice un commit pero esto tiene el primer scraping con playwright, obviamente esto rompe todo el programa pq no lo he terminado pero pullen un commit antiwo si quieren usar la version de selenium

--- scrape_horarios.py
+++ scrape_horarios.py
@@ -1,14 +1,9 @@
+from textwrap import indent
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from playwright.sync_api import sync_playwright
+import re
 
 
 def nurin_scrape(url):
@@ -23,62 +18,36 @@ def nurin_scrape(url):
     return df
 
 
-def nurin_scrape_horario(indice, driver, carrera):
-    # driver = webdriver.Firefox()
-    # driver.get("https://horarios.ulagos.cl/ptomontt/carreras.php")
-    # carrera = driver.find_element(
-    #    By.CSS_SELECTOR, ".select2-selection.select2-selection--single"
-    # )
-    # carrera.click()
-    # carrera_input = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
-    # carrera_input.send_keys(
-    #    "informatica"
-    # )  # por motivos de testeo voy a hacerlo con informatica y luego expandirlo a todas las carreras de PM
-    # carrera_input.send_keys(Keys.RETURN)
-    # set_carrera(driver, carrera)
-    # print("uwu")
-    carrera = carrera.split(" ", 1)[0]
-    time.sleep(2)
-    plan = driver.find_element(By.NAME, "plan_estudio")
-    # print("owo")
-    # plan.click()
-    # print("antes del plan")
-    select = Select(plan)
-    driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", plan)
-    time.sleep(1)
-    select.select_by_index(indice)
-    time.sleep(2)
-    semestres = driver.find_elements(By.CSS_SELECTOR, ".btn.btn-primary")
-    print(str(len(semestres)) + " semestres")
-    i = 1
-    wait = WebDriverWait(driver, 10)
-    for semestre in semestres:
-        a = semestre.text
-        print(a)
-        driver.execute_script(
-            "arguments[0].scrollIntoView({block: 'center'});", semestre
-        )
-        time.sleep(1)
-        semestre.click()
-        # no devuelve la url grrrrrr
-        driver.switch_to.window(driver.window_handles[-1])
-        time.sleep(4)
-        url = driver.current_url
-        print(url)
-        df = nurin_scrape(str(url))
-        df.to_csv(
-            str(carrera) + "-p" + str(indice) + "-n" + str(i) + ".csv",
-            index=False,
-            encoding="utf-8",
-        )
-        time.sleep(1)
+def nurin_scrape_horario(indice, page, carrera, browser):
+    carrera_nombre_a = carrera.replace(" ", "-")
+    carrera_nombre_a = carrera_nombre_a.strip()
+    carrera_nombre_a = re.sub(r"[^\w\-]", "", carrera_nombre_a)
+    # context = browser.new_context()
+    plan = page.locator('[name="plan_estudio"]')
+    plan.select_option(index=indice)
+    semestres = page.locator(".btn.btn-primary")
+    semestres.wait_for(state="visible")
+
+    cant = semestres.count()
+    df_list = []
+    print(f"\n{cant} semestres en {carrera}")
+    i = 0
+    while i < cant:
+        with page.context.expect_page() as new_tab:
+            semestres.nth(i).click()
+            page.wait_for_timeout(500)  # espera 3 segundos
+            pagina_horario = new_tab.value
+            page.wait_for_timeout(500)
+            pagina_horario.wait_for_load_state()
+        url = str(pagina_horario.url)
+        df = nurin_scrape(url)
+        df_list.append(df)
+        # df.to_json(carrera_nombre_a + ".json", orient="records", indent=2)
+        page.wait_for_timeout(500)
+        pagina_horario.close()
+        semestres.nth(i).wait_for(state="visible")
         i += 1
-        # Volver a la página inicial para seleccionar otro semestre
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-        time.sleep(1)
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".btn.btn-primary")))
-        time.sleep(1.25)
+    return df_list
 
 
 def set_carrera(page, nombre_carrera):
@@ -89,30 +58,13 @@ def set_carrera(page, nombre_carrera):
     campo_carrera.wait_for(state="visible")
     campo_carrera.type(str(nombre_carrera))
     campo_carrera.press("Enter")
-    print(f"Obteniendo los planes de estudio ed {nombre_carrera}...")
+    print(f"\nObteniendo los planes de estudio ed {nombre_carrera}...")
     i = get_planes_estudio(page)
     if i == 0:
         print(f"\nalgo salio mal")
     else:
         print(f"\n{i} planes de estudio")
         return i
-    # Selenium#
-    # carrera = driver.find_element(
-    #     By.CSS_SELECTOR, ".select2-selection.select2-selection--single"
-    # )
-    # driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", carrera)
-    # time.sleep(0.5)
-    # carrera.click()
-    # carrera_input = driver.find_element(By.CSS_SELECTOR, ".select2-search__field")
-    # time.sleep(0.3)
-    # carrera_input.clear()
-    # carrera_input.send_keys(str(nombre_carrera) + " ")
-    # carrera_input.send_keys(Keys.RETURN)
-    # time.sleep(1)
-    # print("obteniendo planes de estudio...")
-    # i = get_planes_estudio(driver)
-    # print(str(i - 1) + " planes de estudio")
-    # return i
 
 
 def get_planes_estudio(page):
@@ -120,24 +72,21 @@ def get_planes_estudio(page):
     plan.wait_for(state="visible")
     planes = plan.count()
     return planes
-    # slenderman#
-    # benja weno pal nyacson te tiro to'o el pixi
-    # plan = driver.find_element(By.NAME, "plan_estudio")
-    # select = Select(plan)
-    # print("trabajando...")
-    # driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", plan)
-    # print("retornando planes")
-    # return len(select.options)
 
 
 def get_carreras(page):
     select = page.locator('[name="carrera"]')
     carreras = select.locator("option")
-    # |||selenium|||
-    # carrera = driver.find_element(By.NAME, "carrera")
-    # select = Select(carrera)
-    # carreras = [option.get_attribute("value") for option in select.options]
     return carreras
+
+
+def save_json(dfl, carrera):
+    carrera_nombre_a = carrera.strip()
+    carrera_nombre_a = carrera_nombre_a.replace(" ", "-")
+    carrera_nombre_a = carrera_nombre_a.replace("/", "-")
+    df = pd.concat(dfl, ignore_index=True)
+    df.to_json(str(carrera_nombre_a) + ".json", orient="records", indent=2)
+    # return df
 
 
 with sync_playwright() as p:
@@ -146,7 +95,9 @@ with sync_playwright() as p:
     )  # para no tener interfaz grafica :emoticon dinero:
     page = browser.new_page()
     page.goto("https://horarios.ulagos.cl/ptomontt/carreras.php")
-    i = get_carreras(page)
-    print(i.all_text_contents())
-    set_carrera(page, i.all_text_contents()[1])
+    i = get_carreras(page).all_text_contents()[1]
+    print(i)
+    indice = set_carrera(page, i)
+    dfl = nurin_scrape_horario(indice, page, i, browser)
+    save_json(dfl, str(i))
     page.close()
