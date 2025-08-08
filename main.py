9f4c28f15f24db2d38933d420f035fab3af6f378
Nurodayo <cianuroicecream@gmail.com>
2025-08-07 20:33:41 -0400

    distro hoppin brrr

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 from scrape_horarios import *
-import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
@@ -17,27 +9,23 @@ with sync_playwright() as p:
     )  # para no tener interfaz grafica :emoticon dinero:
     page = browser.new_page()
     page.goto("https://horarios.ulagos.cl/ptomontt/carreras.php")
+    carreras = get_carreras(page)
+    carreras_list = carreras.all_text_contents()
+    carreras_list.pop(0)
+    print(carreras_list)
+    print("Mandandole DDos a la ULakes")
+
+    for carrera in carreras_list:
+        print("carrera: " + str(carrera))
+        i = int(set_carrera(page, carrera))
+        j = 1
+        while j < (i + 1):
+            print("Plan #:" + str(j))
+            df = nurin_scrape_horario(j, page, str(carrera))
+            save_json(df, str(carrera), j)
+            j += 1
+
+    # fin del codigo
     page.close()
 
 # reminiscences of selenium version
-if 1 == 0:  # solo tengo este if para ver la syntax highligh
-    driver = webdriver.Firefox()
-    driver.get("https://horarios.ulagos.cl/ptomontt/carreras.php")
-    carreras = get_carreras(driver)
-    carreras.pop(0)
-    print(carreras)
-    print("Mandandole DDoS a la ULA")
-    for carrera in carreras:
-        # driver.get("https://horarios.ulagos.cl/ptomontt/carreras.php")
-        time.sleep(0.5)
-        print("carrera " + str(carrera))
-        i = set_carrera(driver, str(carrera))
-        j = 1
-        while j < i:
-            print("plan " + str(j))
-            nurin_scrape_horario(j, driver, carrera)
-            time.sleep(1)
-            j += 1
-        time.sleep(0.1)
-
-    driver.quit()
--- scrape_horarios.py
+++ scrape_horarios.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from playwright.sync_api import sync_playwright
 import re
+import time
 
 
 def nurin_scrape(url):
@@ -18,7 +19,7 @@ def nurin_scrape(url):
     return df
 
 
-def nurin_scrape_horario(indice, page, carrera, browser):
+def nurin_scrape_horario(indice, page, carrera):
     carrera_nombre_a = carrera.replace(" ", "-")
     carrera_nombre_a = carrera_nombre_a.strip()
     carrera_nombre_a = re.sub(r"[^\w\-]", "", carrera_nombre_a)
@@ -35,7 +36,7 @@ def nurin_scrape_horario(indice, page, carrera, browser):
     while i < cant:
         with page.context.expect_page() as new_tab:
             semestres.nth(i).click()
-            page.wait_for_timeout(500)  # espera 3 segundos
+            page.wait_for_timeout(500)
             pagina_horario = new_tab.value
             page.wait_for_timeout(500)
             pagina_horario.wait_for_load_state()
@@ -54,6 +55,7 @@ def set_carrera(page, nombre_carrera):
     carrera = page.locator(".select2-selection.select2-selection--single")
     carrera.wait_for(state="visible")
     carrera.click()
+    time.sleep(2)  # a veces clickea y los semestres no cargan
     campo_carrera = page.locator(".select2-search__field")
     campo_carrera.wait_for(state="visible")
     campo_carrera.type(str(nombre_carrera))
@@ -62,6 +64,7 @@ def set_carrera(page, nombre_carrera):
     i = get_planes_estudio(page)
     if i == 0:
         print(f"\nalgo salio mal")
+        return 0
     else:
         print(f"\n{i} planes de estudio")
         return i
@@ -80,24 +83,26 @@ def get_carreras(page):
     return carreras
 
 
-def save_json(dfl, carrera):
+def save_json(dfl, carrera, indice):
     carrera_nombre_a = carrera.strip()
     carrera_nombre_a = carrera_nombre_a.replace(" ", "-")
     carrera_nombre_a = carrera_nombre_a.replace("/", "-")
     df = pd.concat(dfl, ignore_index=True)
-    df.to_json(str(carrera_nombre_a) + ".json", orient="records", indent=2)
+    df.to_json(
+        str(carrera_nombre_a) + str(indice) + ".json", orient="records", indent=2
+    )
     # return df
 
 
-with sync_playwright() as p:
-    browser = p.firefox.launch(
-        headless=False
-    )  # para no tener interfaz grafica :emoticon dinero:
-    page = browser.new_page()
-    page.goto("https://horarios.ulagos.cl/ptomontt/carreras.php")
-    i = get_carreras(page).all_text_contents()[1]
-    print(i)
-    indice = set_carrera(page, i)
-    dfl = nurin_scrape_horario(indice, page, i, browser)
-    save_json(dfl, str(i))
-    page.close()
+# with sync_playwright() as p:
+#  browser = p.firefox.launch(
+#      headless=False
+#  )  # para no tener interfaz grafica :emoticon dinero:
+#  page = browser.new_page()
+#  page.goto("https://horarios.ulagos.cl/ptomontt/carreras.php")
+#  i = get_carreras(page).all_text_contents()[1]
+#  print(i)
+#  indice = set_carrera(page, i)
+#  dfl = nurin_scrape_horario(indice, page, i)
+#  save_json(dfl, str(i), 1)
+#  page.close()
